dbfa16c544546352f86bbfab9f4033b7368312d4
Mohamad Fikri <146539030+mofasa798@users.noreply.github.com>
2025-10-05 21:15:52 +0700

    implement responsive header with hamburger menu; add navbar fixed behavior on scroll

---
+++ dist/js/script.js
@@ -0,0 +1,24 @@
+// navbar fixed
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
+  const toTop = document.querySelector("#to-top");
+    if (window.pageYOffset > fixedNav) {
+        header.classList.add("navbar-fixed");
+        toTop.classList.remove("hidden");
+        toTop.classList.add("flex");
+    } else {
+        header.classList.remove("navbar-fixed");
+        toTop.classList.add("hidden");
+        toTop.classList.remove("flex");
+    }
+};
+
+// hamburger menu
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
+
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("hamburger-active");
+    navMenu.classList.toggle("hidden");
+});
