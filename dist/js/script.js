5e13c8c396abf090c163f540f7a5c460bc556160
Mohamad Fikri <146539030+mofasa798@users.noreply.github.com>
2025-10-10 20:33:58 +0700

    fix: update z-index for navbar-fixed class to ensure proper stacking order style: unify quote styles in JavaScript for consistency

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,24 +1,24 @@
 // navbar fixed
 window.onscroll = function () {
-  const header = document.querySelector("header");
+  const header = document.querySelector('header');
   const fixedNav = header.offsetTop;
-  const toTop = document.querySelector("#to-top");
+  const toTop = document.querySelector('#to-top');
     if (window.pageYOffset > fixedNav) {
-        header.classList.add("navbar-fixed");
-        toTop.classList.remove("hidden");
-        toTop.classList.add("flex");
+        header.classList.add('navbar-fixed');
+        toTop.classList.remove('hidden');
+        toTop.classList.add('flex');
     } else {
-        header.classList.remove("navbar-fixed");
-        toTop.classList.add("hidden");
-        toTop.classList.remove("flex");
+        header.classList.remove('navbar-fixed');
+        toTop.classList.add('hidden');
+        toTop.classList.remove('flex');
     }
 };
 
 // hamburger menu
-const hamburger = document.querySelector("#hamburger");
+const hamburger = document.querySelector('#hamburger');
 const navMenu = document.querySelector("#nav-menu");
 
-hamburger.addEventListener("click", function () {
-  hamburger.classList.toggle("hamburger-active");
-    navMenu.classList.toggle("hidden");
+hamburger.addEventListener('click', function () {
+  hamburger.classList.toggle('hamburger-active');
+    navMenu.classList.toggle('hidden');
 });
