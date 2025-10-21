1545e9247a10a4209d0ded72d45ed48eb143d007
Mohamad Fikri <146539030+mofasa798@users.noreply.github.com>
2025-10-21 21:01:13 +0700

    restruktur code dan juga menambah beberapa fitur

--- dist/js/script.js
+++ dist/js/script.js
@@ -22,3 +22,30 @@ hamburger.addEventListener('click', function () {
   hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden');
 });
+
+// klik diluar hamburger menu
+window.addEventListener('click', function (e) {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove('hamburger-active');
+    navMenu.classList.add('hidden');
+  }
+});
+
+// dark mode toggle
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+darkToggle.addEventListener('click', function () {
+  if (darkToggle.checked) {
+    html.classList.add('dark');
+    localStorage.theme = 'dark';
+  } else {
+    html.classList.remove('dark');
+    localStorage.theme = 'light';
+  }
+});
+// pindah posisi toggle sesuai mode
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = false;
+}
